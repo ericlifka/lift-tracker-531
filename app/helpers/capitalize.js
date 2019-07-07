900d447f617b1a517068b41be3906ce7e0b6455f
Eric Lifka <eric.lifka@genesys.com>
2019-07-07 19:31:20 -0400

    add a title bar component to manage the settings button and back button

---
+++ app/helpers/capitalize.js
@@ -0,0 +1,8 @@
+import { helper } from '@ember/component/helper';
+import { capitalize as cap } from '@ember/string';
+
+export function capitalize(params) {
+  return cap(params[ 0 ]);
+}
+
+export default helper(capitalize);
