b661f706c92e8e48e8b2773ea635bc44c125ba9a
Eric Lifka <eric.lifka@genesys.com>
2019-07-14 14:55:01 -0400

    implement editing for bar weight and rounding threshold

--- app/components/settings-bar-loading-control.js
+++ app/components/settings-bar-loading-control.js
@@ -1,5 +1,34 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: [ 'settings-bar-loading-control' ]
+  classNames: [ 'settings-bar-loading-control' ],
+
+  editing: false,
+
+  commonBarWeights: [ 15, 25, 35, 45, 55 ],
+  commonRoundingThresholds: [ 1, 2.5, 5, 10 ],
+
+  actions: {
+    showEdit() {
+      this.set('editing', true);
+    },
+
+    cancelEdit() {
+      this.get('model').rollbackAttributes();
+      this.set('editing', false);
+    },
+
+    saveEdit() {
+      this.get('model').save();
+      this.set('editing', false);
+    },
+
+    setBarWeight(weight) {
+      this.set('model.bar', weight);
+    },
+
+    setRoundingWeight(weight) {
+      this.set('model.rounding', weight);
+    }
+  }
 });
