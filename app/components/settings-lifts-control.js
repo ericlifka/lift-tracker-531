990df2b05f94c50d1903b46e11b87fa938f354f0
Eric Lifka <eric.lifka@genesys.com>
2019-07-13 22:59:04 -0400

    split editing/display into more logical sections and redo styles for display side

--- app/components/settings-lifts-control.js
+++ app/components/settings-lifts-control.js
@@ -1,5 +1,19 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: [ 'settings-lifts-control' ]
+  classNames: [ 'settings-lifts-control' ],
+
+  editingLifts: false,
+
+  actions: {
+    showEdit() {
+      this.set('editingLifts', true);
+    },
+    saveEdit() {
+      this.set('editingLifts', false);
+    },
+    cancelEdit() {
+      this.set('editingLifts', false);
+    }
+  }
 });
