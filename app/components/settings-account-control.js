04752d6c162b8f9ca563e47b7736beac8cf7aa6b
Eric Lifka <eric.lifka@genesys.com>
2019-07-13 22:07:43 -0400

    redo account controls

--- app/components/settings-account-control.js
+++ app/components/settings-account-control.js
@@ -1,5 +1,14 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  classNames: [ 'settings-account-control' ]
+  classNames: [ 'settings-account-control' ],
+
+  session: service(),
+
+  actions: {
+    signOut() {
+      return this.get('session').invalidate();
+    }
+  }
 });
