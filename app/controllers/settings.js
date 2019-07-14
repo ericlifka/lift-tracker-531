845a6525dd470f3576f195d9ae81068388044ac3
Eric Lifka <eric.lifka@genesys.com>
2019-07-13 21:48:45 -0400

    tabs!

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -1,72 +1,97 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
-import lift from '../routes/lift';
+// import { inject as service } from '@ember/service';
+// import lift from '../routes/lift';
 
 export default Controller.extend({
-  store: service(),
-  session: service(),
+  // store: service(),
+  // session: service(),
 
-  editingLifts: false,
-  newLiftName: '',
-  newLiftMax: '',
+  // editingLifts: false,
+  // newLiftName: '',
+  // newLiftMax: '',
+  showAccountTab: true,
+  showLiftsTab: false,
+  showBarLoadingTab: false,
 
   actions: {
-    signOut() {
-      return this.get('session').invalidate();
+    showAccountTab() {
+      this.setProperties({
+        showAccountTab: true,
+        showLiftsTab: false,
+        showBarLoadingTab: false,
+      });
     },
-
-    showliftInput() {
-      this.set('editingLifts', true);
+    showLiftsTab() {
+      this.setProperties({
+        showAccountTab: false,
+        showLiftsTab: true,
+        showBarLoadingTab: false,
+      });
     },
-
-    saveNewLift(name, max) {
-      if (!name || !max) {
-        return;
-      }
-
-      let userId = this.session.get('data.authenticated.user.uid');
-      let newLift = this.store.createRecord('lift', { name, max, userId });
-      newLift.save()
-        .then(() => {
-          this.clearNewLiftForm();
-        })
-        .catch(error => {
-          console.error('Error creating lift: ', error);
-        });
-    },
-
-    cancelLiftChanges() {
-      this.set('editingLifts', false);
-    },
-
-    cancelNewLift() {
-      this.clearNewLiftForm();
-    },
-
-    increaseLift(lift) {
-      lift.set('max', lift.get('max') + 5);
-      lift.save();
-    },
-
-    decreaseLift(lift) {
-      lift.set('max', lift.get('max') - 5);
-      lift.save();
-    },
-
-    decreaseBar(barLoading) {
-      barLoading.set('bar', barLoading.get('bar') - 5);
-      barLoading.save();
-    },
-
-    increaseBar(barLoading) {
-      barLoading.set('bar', barLoading.get('bar') + 5);
-      barLoading.save();
+    showBarLoadingTab() {
+      this.setProperties({
+        showAccountTab: false,
+        showLiftsTab: false,
+        showBarLoadingTab: true,
+      });
     }
+
+    // signOut() {
+    //   return this.get('session').invalidate();
+    // },
+
+    // showliftInput() {
+    //   this.set('editingLifts', true);
+    // },
+
+    // saveNewLift(name, max) {
+    //   if (!name || !max) {
+    //     return;
+    //   }
+
+    //   let userId = this.session.get('data.authenticated.user.uid');
+    //   let newLift = this.store.createRecord('lift', { name, max, userId });
+    //   newLift.save()
+    //     .then(() => {
+    //       this.clearNewLiftForm();
+    //     })
+    //     .catch(error => {
+    //       console.error('Error creating lift: ', error);
+    //     });
+    // },
+
+    // cancelLiftChanges() {
+    //   this.set('editingLifts', false);
+    // },
+
+    // cancelNewLift() {
+    //   this.clearNewLiftForm();
+    // },
+
+    // increaseLift(lift) {
+    //   lift.set('max', lift.get('max') + 5);
+    //   lift.save();
+    // },
+
+    // decreaseLift(lift) {
+    //   lift.set('max', lift.get('max') - 5);
+    //   lift.save();
+    // },
+
+    // decreaseBar(barLoading) {
+    //   barLoading.set('bar', barLoading.get('bar') - 5);
+    //   barLoading.save();
+    // },
+
+    // increaseBar(barLoading) {
+    //   barLoading.set('bar', barLoading.get('bar') + 5);
+    //   barLoading.save();
+    // }
   },
 
-  clearNewLiftForm() {
-    this.set('newLiftName', '');
-    this.set('newLiftMax', '');
-    this.set('editingLifts', false);
-  }
+  // clearNewLiftForm() {
+  //   this.set('newLiftName', '');
+  //   this.set('newLiftMax', '');
+  //   this.set('editingLifts', false);
+  // }
 });
